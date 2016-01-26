8066e136850112379cdc42e8e037621e4ab6a1be
Daniel Langr <daniel.langr@gmail.com>
2016-01-26 14:12:17 +0100

    iteration over blocks accomplished

--- preprocessing/test.cpp
+++ preprocessing/test.cpp
@@ -3,6 +3,7 @@
 #include <cstdint>
 #include <cstdlib>
 #include <iostream>
+#include <string>
 #include <utility>
 #include <vector>
 
@@ -20,31 +21,34 @@ const std::string cyan("\033[0;36m");
 const std::string magenta("\033[0;35m");
 const std::string reset("\033[0m");
 
-int main(int argc, char* argv[])
+// data
+uintmax_t nnz;
+std::vector<uint32_t> rows, cols;
+std::vector<double> vals_re, vals_im;
+std::vector<intmax_t> vals_int;
+
+// reader
+using mm_reader = matrix_market_reader<>;
+mm_reader reader(&std::cout);
+
+void read(const std::string& filename) 
 {
-    using matrix_market_reader = matrix_market_reader<>;
-
-    timer_type timer(timer_type::start_now);
-
-    std::cout << "Reading matrix file: " << argv[1] << std::endl;
-    matrix_market_reader reader(&std::cout);
-    reader.open(argv[1]);
-
-    uintmax_t nnz = reader.nnz;
-    std::vector<uint32_t> rows(nnz), cols(nnz);
-    std::vector<double> vals_re, vals_im;
-    std::vector<intmax_t> vals_int;
+    reader.open(filename);
+    
+    nnz = reader.nnz;
+    rows.resize(nnz);
+    cols.resize(nnz);
 
     switch (reader.type) {
-        case matrix_market_reader::INTEGER:
+        case mm_reader::INTEGER:
             vals_int.resize(nnz);
             break;
 
-        case matrix_market_reader::REAL:
+        case mm_reader::REAL:
             vals_re.resize(nnz);
             break;
 
-        case matrix_market_reader::COMPLEX:
+        case mm_reader::COMPLEX:
             vals_re.resize(nnz);
             vals_im.resize(nnz);
             break;
@@ -63,15 +67,15 @@ int main(int argc, char* argv[])
         cols[k] = col;
 
         switch (reader.type) {
-            case matrix_market_reader::INTEGER:
+            case mm_reader::INTEGER:
                 vals_int[k] = val_int;
                 break;
 
-            case matrix_market_reader::REAL:
+            case mm_reader::REAL:
                 vals_re[k] = val_re;
                 break;
 
-            case matrix_market_reader::COMPLEX:
+            case mm_reader::COMPLEX:
                 vals_re[k] = val_re;
                 vals_im[k] = val_im;
                 break;
@@ -80,18 +84,15 @@ int main(int argc, char* argv[])
                 break;
         }
     }
+}
 
-    timer.stop();
-    std::cout << "Matrix reading time: " << yellow << timer.seconds() << reset << " [s]" << std::endl;
+void iterate(const uintmax_t bsk, int num_threads)
+{
 
-    const uintmax_t bsk = std::atoi(argv[2]);
-    std::cout << "Block size power: " << cyan << bsk << reset << std::endl;
-    std::cout << "Block size: " << cyan << (1 << bsk) << reset <<std::endl;
+    // sort in parallel
+    omp_set_num_threads(num_threads);
 
-    int num_threads = std::atoi(argv[3]);
-    std::cout << "Number of threads: " << cyan << num_threads << reset << std::endl;
-
-    auto comp = [&rows, &cols, bsk] (std::size_t i, std::size_t j) {
+    auto comp = [bsk](std::size_t i, std::size_t j) {
         uintmax_t Ii = rows[i] >> bsk;
         uintmax_t Ij = rows[j] >> bsk;
         uintmax_t Ji = cols[i] >> bsk;
@@ -102,54 +103,127 @@ int main(int argc, char* argv[])
         return false;
     };
 
-    auto swap_pat = [&rows, &cols] (std::size_t i, std::size_t j) {
-        std::swap(rows[i], rows[j]);
-        std::swap(cols[i], cols[j]);
-    };
-
-    auto swap_int = [&rows, &cols, &vals_int] (std::size_t i, std::size_t j) {
-        std::swap(rows[i], rows[j]);
-        std::swap(cols[i], cols[j]);
-        std::swap(vals_int[i], vals_int[j]);
-    };
-
-    auto swap_real = [&rows, &cols, &vals_re] (std::size_t i, std::size_t j) {
-        std::swap(rows[i], rows[j]);
-        std::swap(cols[i], cols[j]);
-        std::swap(vals_re[i], vals_re[j]);
-    };
-
-    auto swap_comp = [&rows, &cols, &vals_re, &vals_im] (std::size_t i, std::size_t j) {
-        std::swap(rows[i], rows[j]);
-        std::swap(cols[i], cols[j]);
-        std::swap(vals_re[i], vals_re[j]);
-        std::swap(vals_im[i], vals_im[j]);
-    };
-
-    omp_set_num_threads(num_threads);
-
-    timer.start();
     switch (reader.type) {
-        case matrix_market_reader::PATTERN:
-            #pragma omp parallel 
-            aqsort::parallel_sort(nnz, &comp, &swap_pat);
+        case mm_reader::PATTERN:
+            {
+                auto swap_pat = [](std::size_t i, std::size_t j) {
+                    std::swap(rows[i], rows[j]);
+                    std::swap(cols[i], cols[j]);
+                };
+
+                #pragma omp parallel 
+                aqsort::parallel_sort(nnz, &comp, &swap_pat);
+            }
             break;
 
-        case matrix_market_reader::INTEGER:
-            #pragma omp parallel 
-            aqsort::parallel_sort(nnz, &comp, &swap_int);
+        case mm_reader::INTEGER:
+            {
+                auto swap_int = [](std::size_t i, std::size_t j) {
+                    std::swap(rows[i], rows[j]);
+                    std::swap(cols[i], cols[j]);
+                    std::swap(vals_int[i], vals_int[j]);
+                };
+
+                #pragma omp parallel 
+                aqsort::parallel_sort(nnz, &comp, &swap_int);
+            }
             break;
 
-        case matrix_market_reader::REAL:
-            #pragma omp parallel 
-            aqsort::parallel_sort(nnz, &comp, &swap_real);
+        case mm_reader::REAL:
+            {
+                auto swap_real = [](std::size_t i, std::size_t j) {
+                    std::swap(rows[i], rows[j]);
+                    std::swap(cols[i], cols[j]);
+                    std::swap(vals_re[i], vals_re[j]);
+                };
+
+                #pragma omp parallel 
+                aqsort::parallel_sort(nnz, &comp, &swap_real);
+            }
             break;
 
-        case matrix_market_reader::COMPLEX:
-            #pragma omp parallel 
-            aqsort::parallel_sort(nnz, &comp, &swap_comp);
+        case mm_reader::COMPLEX:
+            {
+                auto swap_comp = [](std::size_t i, std::size_t j) {
+                    std::swap(rows[i], rows[j]);
+                    std::swap(cols[i], cols[j]);
+                    std::swap(vals_re[i], vals_re[j]);
+                    std::swap(vals_im[i], vals_im[j]);
+                };
+
+                #pragma omp parallel 
+                aqsort::parallel_sort(nnz, &comp, &swap_comp);
+            }
             break;
     }
+
+    // iterate in parallel
+    uintmax_t tb[num_threads + 1];
+    tb[0] = 0;
+    tb[num_threads] = nnz;
+    const uintmax_t s = 1UL << bsk;
+
+    omp_set_num_threads(num_threads);
+    #pragma omp parallel
+    {
+        int t = omp_get_thread_num();
+
+        // find boundaries for threads
+        if (t > 0) {
+            uintmax_t l = (nnz * t) / num_threads;
+            uintmax_t I = rows[l] >> bsk;
+            uintmax_t J = cols[l] >> bsk;
+            uintmax_t I_, J_;
+
+            do {
+                l++;
+                I_ = rows[l] >> bsk;
+                J_ = cols[l] >> bsk;
+            } while ((l < (nnz - 1)) && (I_ == I) && (J_ == J));
+            tb[t] = l;
+        }
+
+        #pragma omp barrier
+
+        uintmax_t l1 = tb[t];
+        uintmax_t I = rows[l1] >> bsk;
+        uintmax_t J = cols[l1] >> bsk;
+
+        for (uintmax_t l = tb[t]; l < tb[t + 1] - 1; l++) {
+            uintmax_t I_ = rows[l] >> bsk;
+            uintmax_t J_ = cols[l] >> bsk;
+            if ((I_ != I) || (J_ != J)) {
+                // process block with indexes between l1 and (l - 1) inclusive
+
+                l1 = l;
+                I = I_;
+                J = J_;
+            }
+        }
+
+        // process block with indexes between l1 and (tb[t + 1] - 1) inclusive
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    // read matrix from file
+    std::string filename(argv[1]);
+    std::cout << "Reading matrix file: " << filename << std::endl;
+    timer_type timer(timer_type::start_now);
+    read(filename);
     timer.stop();
-    std::cout << "Sorting of elements: " << yellow << timer.seconds() << reset << std::endl;
+    std::cout << "Matrix reading time: " << yellow << timer.seconds() << reset << " [s]" << std::endl;
+
+    const uintmax_t bsk = std::atoi(argv[2]);
+    std::cout << "Block size power: " << cyan << bsk << reset << std::endl;
+    std::cout << "Block size: " << cyan << (1 << bsk) << reset <<std::endl;
+
+    int num_threads = std::atoi(argv[3]);
+    std::cout << "Number of threads: " << cyan << num_threads << reset << std::endl;
+
+    timer.start();
+    iterate(bsk, num_threads);
+    timer.stop();
+    std::cout << "Iteration over blocks: " << green << timer.seconds() << reset << std::endl;
 }
