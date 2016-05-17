24c46df6085cd9caf68665efdb62709a4e265876
Daniel Langr <daniel.langr@gmail.com>
2016-05-17 14:57:40 +0200

    results moved to experiments, process script updated

--- block_statistics/experiments/process.sh
+++ block_statistics/experiments/process.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+ABHSF_ROOT="${HOME}/projects/abhsf"
+
 cyan='\033[0;36m'
 reset='\033[0m'
+
+mkdir -p results
+cd results
 
 while read line
 do
@@ -21,11 +26,13 @@ do
    rm -rf ${mat}-temp
    
    cd $mat
-   ../../block_stats ${mat}.mtx
+   $ABHSF_ROOT/block_statistics/block_stats ${mat}.mtx
    rm ${mat}.mtx
    cd ..
 
    cd $mat
-   ../../msmf
+   $ABHSF_ROOT/block_statistics/msmf
    cd ..
 done < $1
+
+cd ..
