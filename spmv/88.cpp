f3be280b848672cf1245395f6cbd32d2bea23af0
Daniel Langr <daniel.langr@gmail.com>
2016-10-10 16:35:19 +0200

    SpMV experimental programs added

---
+++ spmv/88.cpp
@@ -0,0 +1,535 @@
+#include <algorithm>
+#include <cassert>
+#include <cstdint>
+#include <cstdlib>
+#include <iomanip>
+#include <iostream>
+#include <numeric>
+#include <set>
+#include <stdexcept>
+#include <string>
+#include <random>
+#include <tuple>
+#include <utility>
+#include <vector>
+
+#include <mkl.h>
+
+#include <abhsf/utils/colors.h>
+#include <abhsf/utils/timer.h>
+
+using timer_type = chrono_timer<>;
+
+//using real = float;
+using real_type = double;
+
+// block size fixed 8x8 for now
+static const uint32_t block_size = 8;
+static const uint32_t block_size_exp = 3;
+static_assert(block_size == (1UL << block_size_exp), "Block size does not match its exponent.");
+
+// number of blocks per block row / block column
+// (num_blocks * num_blocks * block_size * block_size) elements must fit into memory
+// (num_blocks * num_blocks) elements must not fit into cache
+static const uint32_t num_blocks = 4096;
+
+static const int num_experiments = 80;
+static const int num_iterations = 40;
+static const int warm_up_iterations = 5;
+
+class block_generator
+{
+    public:
+        block_generator() : gen_(0x0498bc04) { } // same seed to generate same "random" blocks 
+
+        void generate_random(int nnz)
+        {
+            assert((nnz > 0) && (nnz <= block_size * block_size));
+
+            set_.clear();
+
+            std::random_device rd;
+            std::mt19937 gen(rd());
+            std::uniform_int_distribution<> dis(0, block_size - 1);
+
+            int k = 0;
+            while (k < nnz) {
+                uint32_t row = dis(gen);
+                uint32_t col = dis(gen);
+
+                if (set_.find(std::make_pair(row, col)) == set_.end()) {
+                    set_.emplace(row, col);
+                    k++;
+                }
+            }
+
+            finalize();
+        }
+
+        void generate_diagonal()
+        {
+            set_.clear();
+            for (int k = 0; k < block_size; k++)
+                set_.emplace(k, k);
+
+            finalize();
+        }
+
+        void generate_single_row()
+        {
+            set_.clear();
+            for (int k = 0; k < block_size; k++)
+                set_.emplace(0, k);
+
+            finalize();
+        }
+
+        void generate_single_col()
+        {
+            set_.clear();
+            for (int k = 0; k < block_size; k++)
+                set_.emplace(k, 0);
+
+            finalize();
+        }
+
+        const std::vector<real_type>& nonzero_values() const { return nonzero_values_; }
+        const std::vector<real_type>& values() const { return values_; }
+        const std::vector<uint8_t>& indices() const { return indices_; }
+        uint64_t bitmap() const { return bitmap_; }
+
+    private:
+        void finalize()
+        {
+            nonzero_values_.clear();
+            values_.clear();
+            indices_.clear();
+            bitmap_ = 0;
+
+            auto iter = set_.cbegin();
+
+            for (int row = 0; row < block_size; row++) {
+                for (int col = 0; col < block_size; col++) {
+                    if ((iter->first == row) && (iter->second == col)) {
+                        nonzero_values_.emplace_back(1.0);
+                        values_.emplace_back(1.0);
+
+                        static_assert(block_size_exp < 4, "Block size expected to be less or equal than 16.");
+                        indices_.emplace_back((row << 4) + col);
+
+                        static_assert(block_size == 8, "Block size expected to equal 8.");
+                        bitmap_ |= 1UL << ((8 * row) + col);
+
+                        ++iter;
+                    }
+                    else 
+                        values_.emplace_back(0.0);
+                }
+            }
+        }
+
+        std::set<std::pair<uint32_t, uint32_t>> set_;
+
+        std::vector<real_type> nonzero_values_;
+        std::vector<real_type> values_;
+
+        std::vector<uint8_t> indices_; // row and column indices
+        uint64_t bitmap_; // bitmap describing block structure
+
+        std::mt19937 gen_;
+};
+
+class block_coo_matrix_t
+{
+    public:
+        void assemble(const block_generator& bgen)
+        {
+            brows_nnz_.clear();
+            bcol_inds_.clear();
+            values_.clear();
+            bnnz_.clear();
+            indices_.clear();
+
+            const auto& nonzero_values = bgen.nonzero_values();
+            const auto& indices = bgen.indices();
+
+            brows_nnz_.reserve(num_blocks);
+            bcol_inds_.reserve(num_blocks * num_blocks);
+            values_.reserve(num_blocks * num_blocks * nonzero_values.size());
+            bnnz_.reserve(num_blocks * num_blocks);
+            indices_.reserve(num_blocks * num_blocks * indices.size());
+
+            for (int brow = 0; brow < num_blocks; brow++) {
+                brows_nnz_.emplace_back(num_blocks);
+
+                for (int bcol = 0; bcol < num_blocks; bcol++) {
+                    bcol_inds_.emplace_back(bcol);
+
+                    values_.insert(values_.end(), nonzero_values.cbegin(), nonzero_values.cend());
+                    bnnz_.emplace_back(bgen.indices().size());
+                    indices_.insert(indices_.end(), indices.cbegin(), indices.cend());
+                }
+            }
+        }
+
+        void spmv(const std::vector<real_type>& x, std::vector<real_type>& y)
+        {
+            auto bcol_iter = bcol_inds_.cbegin();
+            auto values_iter = values_.cbegin();
+            auto bnnz_iter = bnnz_.cbegin();
+            auto indices_iter = indices_.cbegin();
+
+            // optimizations? vectorization, software prefetching, ...
+
+            for (uint32_t brow = 0; brow < num_blocks; brow++) {
+                for (uint32_t j = 0; j < brows_nnz_[brow]; j++) {
+                    uint32_t bcol = *bcol_iter;
+
+                    for (uint32_t k = 0; k < *bnnz_iter; k++) {
+                        uint32_t lrow = *indices_iter >> 4;
+                        uint32_t lcol = *indices_iter & 0x0F;
+
+                        y[brow * block_size + lrow] += *values_iter * x[bcol * block_size + lcol];
+
+                        ++values_iter;
+                        ++indices_iter;
+                    }
+                    ++bcol_iter;
+                    ++bnnz_iter;
+                }
+            }
+
+        }
+
+    private:
+        std::vector<uint16_t> brows_nnz_; // number of nonzero block per block rows
+        std::vector<uint16_t> bcol_inds_; // their block column indices
+        std::vector<real_type> values_; // values of nonzero elements
+        std::vector<uint8_t> bnnz_; // number of nonzero elements per each nonzero block
+        std::vector<uint8_t> indices_; // compressed row and column local in-block inidces
+};
+
+class block_bitmap_matrix_t
+{
+    public:
+        void assemble(const block_generator& bgen)
+        {
+            brows_nnz_.clear();
+            bcol_inds_.clear();
+            values_.clear();
+            bitmap_.clear();
+
+            const auto& nonzero_values = bgen.nonzero_values();
+
+            brows_nnz_.reserve(num_blocks);
+            bcol_inds_.reserve(num_blocks * num_blocks);
+            values_.reserve(num_blocks * num_blocks * nonzero_values.size());
+            bitmap_.reserve(num_blocks * num_blocks);
+
+            for (int brow = 0; brow < num_blocks; brow++) {
+                brows_nnz_.emplace_back(num_blocks);
+
+                for (int col = 0; col < num_blocks; col++) {
+                    bcol_inds_.emplace_back(col);
+
+                    values_.insert(values_.end(), nonzero_values.cbegin(), nonzero_values.cend());
+                    bitmap_.emplace_back(bgen.bitmap());
+                }
+            }
+        }
+
+        void spmv(const std::vector<real_type>& x, std::vector<real_type>& y)
+        {
+            auto bcol_iter = bcol_inds_.cbegin();
+            auto values_iter = values_.cbegin();
+            auto bitmap_iter = bitmap_.cbegin();
+
+            // optimizations? vectorization, software prefetching, ...
+
+            for (uint32_t brow = 0; brow < num_blocks; brow++) {
+                for (uint32_t j = 0; j < brows_nnz_[brow]; j++) {
+                    uint32_t bcol = *bcol_iter;
+                    uint64_t bitmap = *bitmap_iter;
+
+                    static_assert(block_size == 8, "Block size expected to equal 8.");
+                    for (uint32_t lrow = 0; lrow < 8; lrow++) {
+                        uint8_t row_bitmap = bitmap >> 8 * (7 - lrow);
+                        if (row_bitmap > 0) {
+                            for (int32_t lcol = 7; lcol >= 0; lcol--) {
+                                if (((row_bitmap >> (7 - lcol)) & 1) == 1) {
+                                    y[brow * 8 + lrow] += *values_iter * x[bcol * 8 + lcol];
+                                    ++values_iter;
+                                }
+                            }
+                        }
+                    }
+                    
+                    ++bcol_iter;
+                    ++bitmap_iter;
+                }
+            }
+
+        }
+
+    private:
+        std::vector<uint16_t> brows_nnz_; // same as for block_coo_matrix
+        std::vector<uint16_t> bcol_inds_; // same as for block_coo_matrix
+        std::vector<real_type> values_; // same as for block_coo_matrix
+        std::vector<uint64_t> bitmap_; // bitmap for each block in lexicographical order
+};
+
+class block_dense_matrix_t
+{
+    public:
+        void assemble(const block_generator& bgen)
+        {
+            brows_nnz_.clear();
+            bcol_inds_.clear();
+            values_.clear();
+
+            const auto& values = bgen.values();
+
+            brows_nnz_.reserve(num_blocks);
+            bcol_inds_.reserve(num_blocks * num_blocks);
+            values_.reserve(num_blocks * num_blocks * block_size * block_size);
+
+            for (int brow = 0; brow < num_blocks; brow++) {
+                brows_nnz_.emplace_back(num_blocks);
+
+                for (int bcol = 0; bcol < num_blocks; bcol++) {
+                    bcol_inds_.emplace_back(bcol);
+                    values_.insert(values_.end(), values.cbegin(), values.cend());
+                }
+            }
+        }
+
+        void spmv(const std::vector<real_type>& x, std::vector<real_type>& y)
+        {
+            auto bcol_iter = bcol_inds_.cbegin();
+            auto values_iter = values_.cbegin();
+
+            for (uint32_t brow = 0; brow < num_blocks; brow++) {
+                for (uint32_t j = 0; j < brows_nnz_[brow]; j++) {
+                    uint32_t bcol = *bcol_iter;
+
+                    for (uint32_t lrow = 0; lrow < block_size; lrow++) {
+                        for (uint32_t lcol = 0; lcol < block_size; lcol++) {
+                            y[brow * block_size + lrow] += *values_iter * x[bcol * block_size + lcol];
+                            ++values_iter;
+                        }
+                    }
+                    
+                    ++bcol_iter;
+                }
+            }
+        }
+
+        void spmv_mkl(const std::vector<real_type>& x, std::vector<real_type>& y)
+        {
+            auto bcol_iter = bcol_inds_.cbegin();
+            real_type* a = values_.data();
+
+            for (uint32_t brow = 0; brow < num_blocks; brow++) {
+                for (uint32_t j = 0; j < brows_nnz_[brow]; j++) {
+                    uint32_t bcol = *bcol_iter;
+
+                    cblas_dgemv(CblasRowMajor, CblasNoTrans, block_size, block_size, 1.0, a, block_size, 
+                           x.data() + bcol * block_size, 1, 0.0, y.data() + brow * block_size, 1); 
+
+                    a += block_size * block_size;
+                    ++bcol_iter;
+                }
+           }
+        }
+
+    private:
+        std::vector<uint16_t> brows_nnz_; // same as for block_coo_matrix
+        std::vector<uint16_t> bcol_inds_; // same as for block_coo_matrix
+        std::vector<real_type> values_; // values for both nonzero and zero elements
+};
+
+// non-blocking CSR matrix with 32-bit indices
+class csr_matrix_t
+{
+    public:
+        void assemble(const block_generator& bgen)
+        {
+            a_.clear();
+            ia_.clear();
+            ja_.clear();
+
+            const auto& nonzero_values = bgen.nonzero_values();
+            const auto& indices = bgen.indices();
+
+            a_.reserve(num_blocks * num_blocks * nonzero_values.size());
+            ia_.reserve(num_blocks * block_size + 1);
+            ja_.reserve(num_blocks * num_blocks * nonzero_values.size());
+
+            using element_t = std::tuple<uint32_t, uint32_t, real_type>;
+            std::vector<element_t> elements;
+            uintmax_t nnz = num_blocks * num_blocks * nonzero_values.size();
+            elements.reserve(nnz);
+
+            for (uint32_t brow = 0; brow < num_blocks; brow++) {
+                for (uint32_t bcol = 0; bcol < num_blocks; bcol++) {
+                    for (int k = 0; k < nonzero_values.size(); k++) {
+                        static_assert(block_size_exp < 4, "Block size expected to be less or equal than 16.");
+                        elements.emplace_back(
+                                brow * block_size + (indices[k] >> 4),
+                                bcol * block_size + (indices[k] & 0x0F),
+                                nonzero_values[k]);
+                    }
+                }
+            }
+
+            // to-CSR conversion
+            std::sort(elements.begin(), elements.end());
+
+            for (size_t k = 0; k < nnz; k++) {
+                a_[k] = std::get<2>(elements[k]);
+                ja_[k] = std::get<1>(elements[k]);
+            }
+
+            ia_[0] = 0;
+            size_t k = 0;
+            size_t row = 0;
+
+            while (k < nnz) {
+                while ((k < nnz) && (row == std::get<0>(elements[k])))
+                    k++;
+
+                row++;
+                ia_[row] = k;
+            }
+
+        }
+
+        void spmv(const std::vector<real_type>& x, std::vector<real_type>& y)
+        {
+            for (MKL_INT row = 0; row < num_blocks * block_size; row++) 
+                for (MKL_INT k = ia_[row]; k < ia_[row + 1]; k++)
+                    y[row] += x[ja_[k]] * a_[k];
+        }
+
+        void spmv_mkl(const std::vector<real_type>& x, std::vector<real_type>& y)
+        {
+            const MKL_INT m = num_blocks * block_size;
+            const char transa = 'N';
+            
+            mkl_cspblas_dcsrgemv(&transa, &m, a_.data(), ia_.data(), ja_.data(), x.data(), y.data());
+        }
+
+    private:
+        std::vector<real_type> a_;
+
+        static_assert(sizeof(MKL_INT) == 4, "Size of MKL_INT must be 4");
+
+        std::vector<MKL_INT> ia_;
+        std::vector<MKL_INT> ja_;
+};
+
+int main(int argc, char* argv[])
+{
+    std::cout << "Blocks size: " << magenta << block_size << reset << std::endl;
+    std::cout << "Number of block rows/columns: " << magenta << num_blocks << reset << std::endl;
+    std::cout << "Number of random experiments: " << magenta << num_experiments << reset << std::endl;
+    std::cout << "Number of iterations: " << magenta << num_iterations << reset << std::endl;
+    std::cout << "Number of warm-up iterations: " << magenta << warm_up_iterations << reset << std::endl;
+
+    // types of matrices / spmv variants
+    enum type_t {
+        block_coo,
+        block_bitmap,
+        block_dense,
+        block_dense_mkl,
+        csr,
+        csr_mkl
+    } type = block_coo;
+
+    if (argc > 1)
+        type = static_cast<type_t>(atol(argv[1]));
+
+    std::cout << "Matrix / SpMV type: " << yellow;
+    switch (type) {
+        case block_coo:       std::cout << "BLOCK COO";       break;
+        case block_bitmap:    std::cout << "BLOCK BITMAP";    break;
+        case block_dense:     std::cout << "BLOCK DENSE";     break;
+        case block_dense_mkl: std::cout << "BLOCK DENSE MKL"; break;
+        case csr:             std::cout << "CSR";             break;
+        case csr_mkl:         std::cout << "CSR MKL";         break;
+        default:              throw std::runtime_error("Unknown matrix/spmv type!");
+    }
+    std::cout << reset << std::endl;
+
+    block_generator bgen;
+
+    std::vector<real_type> x(num_blocks * block_size, 1.0);
+    std::vector<real_type> y(num_blocks * block_size, 0.0);
+
+    block_coo_matrix_t block_coo_matrix;
+    block_bitmap_matrix_t block_bitmap_matrix;
+    block_dense_matrix_t block_dense_matrix;
+    csr_matrix_t csr_matrix;
+
+    for (int bnnz = 1; bnnz <= block_size * block_size; bnnz++) {
+        double mflops_min, mflops_max, mflops_avg, mflops_sum = 0.0;
+
+        for (int exp = 0; exp < num_experiments; exp++) {
+            bgen.generate_random(bnnz);
+
+            switch (type) {
+                case block_coo:       block_coo_matrix.assemble(bgen);    break;
+                case block_bitmap:    block_bitmap_matrix.assemble(bgen); break;
+                case block_dense:
+                case block_dense_mkl: block_dense_matrix.assemble(bgen);  break;
+                case csr:
+                case csr_mkl:         csr_matrix.assemble(bgen);          break;
+                default: break;
+            }
+
+            timer_type timer;
+
+            for (int iter = -warm_up_iterations; iter < num_iterations; iter++) {
+                if (iter == 0)
+                    timer.start();
+
+                switch (type) {
+                    case block_coo:       block_coo_matrix.spmv(x, y);       break;
+                    case block_bitmap:    block_bitmap_matrix.spmv(x, y);    break;
+                    case block_dense:     block_dense_matrix.spmv(x, y);     break;
+                    case block_dense_mkl: block_dense_matrix.spmv_mkl(x, y); break;
+                    case csr:             csr_matrix.spmv(x, y);             break;
+                    case csr_mkl:         csr_matrix.spmv_mkl(x, y);         break;
+                    default: break;
+                }
+            }
+
+            timer.stop();
+            uintmax_t n_flops = 2 * bnnz * num_blocks * num_blocks * num_iterations;
+            double mflops = double(n_flops) / 1.0e6 / timer.seconds();
+
+            if (exp == 0) 
+                mflops_min = mflops_max = mflops_sum = mflops;
+            else {
+                mflops_min = std::min(mflops_min, mflops);
+                mflops_max = std::max(mflops_max, mflops);
+                mflops_sum += mflops;
+            }
+        }
+
+        mflops_avg = mflops_sum / double(num_experiments);
+
+        std::cout
+            << "Block nonzeros = " << cyan << std::right << std::setw(4) << bnnz << reset
+            << ", MFLOP/s: min = " << green << std::right
+                << std::fixed << std::setprecision(2) << std::setw(10) << mflops_min << reset
+            << ", avg = " << green << std::right
+                << std::fixed << std::setprecision(2) << std::setw(10) << mflops_avg << reset
+            << ", max = " << green << std::right
+                << std::fixed << std::setprecision(2) << std::setw(10) << mflops_max << reset
+            << std::endl;
+    }
+
+    std::cout << "y-vector sum: " << red << std::scientific << std::setprecision(15) 
+        << std::accumulate(y.cbegin(), y.cend(), 0.0) << reset << std::endl;
+}
--- spmv/spmv.cpp
+++ spmv/spmv.cpp
@@ -12,6 +12,9 @@
 #include <abhsf/utils/colors.h>
 #include <abhsf/utils/matrix_properties.h>
 #include <abhsf/utils/matrix_market_reader.h>
+#include <abhsf/utils/timer.h>
+
+using timer_type = chrono_timer<>;
 
 using element_t = std::tuple<uint32_t, uint32_t, double>;
 using elements_t = std::vector<element_t>;
@@ -59,38 +62,52 @@ void read_mtx(const std::string& filename, elements_t& elements, matrix_properti
     std::cout << green << "... [DONE]" << reset << std::endl;
 }
 
-/*
-inline uint64_t block_index_powers(const element_t& element, const int k, const int l)
+inline uint64_t block_row(const element_t& element, const int k)
 {
-    uint64_t block_row = (uint64_t)element.first >> k;
-    uint64_t block_col = (uint64_t)element.second >> l;
-    return (block_row << 32) + block_col;
+    return ((uint64_t)(std::get<0>(element))) >> k;
 }
 
-void process_block_size_powers(elements_t& elements, const int k, const int l, map_t& map)
+inline uint64_t block_col(const element_t& element, const int l)
 {
-    assert(elements.size() > 0);
+    return ((uint64_t)(std::get<1>(element))) >> l;
+}
 
+inline uint64_t inblock_row(const element_t& element, const int k)
+{
+    return ((uint64_t)(std::get<0>(element))) & ((1UL << k) - 1);
+}
+
+inline uint64_t inblock_col(const element_t& element, const int l)
+{
+    return ((uint64_t)(std::get<1>(element))) & ((1UL << l) - 1);
+}
+
+void sort_wrt_blocks_lex(elements_t& elements, const int k, const int l)
+{
     auto comp = [k, l](const element_t& lhs, const element_t& rhs) {
-        return block_index_powers(lhs, k, l) < block_index_powers(rhs, k, l);
+        if (block_row(lhs, k) < block_row(rhs, k))
+            return true;
+        else if (block_row(lhs, k) > block_row(rhs, k))
+            return false;
+        // same block row
+
+        if (block_col(lhs, l) < block_col(rhs, l))
+            return true;
+        else if (block_col(lhs, l) > block_col(rhs, l))
+            return false;
+        // same block
+
+        if (inblock_row(lhs, k) < inblock_row(rhs, k))
+            return true;
+        else if (inblock_row(lhs, k) > inblock_row(rhs, k))
+            return false;
+        // same row within same block
+
+        return inblock_col(lhs, l) < inblock_col(rhs, l);
     };
 
     std::sort(elements.begin(), elements.end(), comp);
-
-    auto bi_1 = block_index_powers(elements.front(), k, l);
-    uintmax_t block_nnz = 1;
-    for (auto iter = elements.cbegin() + 1; iter != elements.cend(); ++iter) {
-        auto bi_2 = block_index_powers(*iter, k, l);
-        if (bi_2 != bi_1) {
-            map[block_nnz]++;
-            block_nnz = 0;
-            bi_1 = bi_2;
-        }
-        block_nnz++;
-    }
-    map[block_nnz]++;
 }
-*/
 
 int main(int argc, char* argv[])
 {
@@ -112,4 +129,91 @@ int main(int argc, char* argv[])
 
     // sort lexicographically
     std::sort(elements.begin(), elements.end());
+
+    std::cout << "sizeof(MKL_INT): " << sizeof(MKL_INT) << std::endl;
+
+    // CSR format for Intel MKL
+    std::vector<double> a(nnz_stored);
+    std::vector<MKL_INT> ia(props.m + 1);
+    std::vector<MKL_INT> ja(nnz_stored);
+
+    for (size_t k = 0; k < nnz_stored; k++) {
+        a[k] = std::get<2>(elements[k]);
+        ja[k] = std::get<1>(elements[k]);
+    }
+
+    ia[0] = 0;
+    size_t k = 0;
+    size_t row = 0;
+
+    while (k < nnz_stored) {
+        while ((k < nnz_stored) && (row == std::get<0>(elements[k])))
+            k++;
+
+        row++;
+        ia[row] = k;
+    }
+
+    assert(ia[props.m] == nnz_stored);
+
+    std::vector<double> x(props.n, 1.0);
+    std::vector<double> y(props.m);
+
+    assert(props.m == props.n);
+    const MKL_INT m = props.m;
+    const char transa = 'N';
+
+    static const int n_iters = 200;
+
+    timer_type timer(timer_type::start_now);
+
+    for (int iter = 0; iter < n_iters; iter++) 
+        mkl_cspblas_dcsrgemv(&transa, &m, a.data(), ia.data(), ja.data(), x.data(), y.data());
+
+    timer.stop();
+
+    uintmax_t n_flops = 2 * nnz_all * n_iters;
+    double mega_flops = double(n_flops) / 1.0e6 / timer.seconds();
+    std::cout << "Measured MFLOP/S Intel MKL: " << yellow << mega_flops << reset << std::endl;
+
+    // naive CSR32
+    timer.start();
+
+    for (int iter = 0; iter < n_iters; iter++) {
+        std::fill(y.begin(), y.end(), 0.0);
+
+        for (MKL_INT row = 0; row < m; row++) 
+            for (MKL_INT k = ia[row]; k < ia[row + 1]; k++)
+                y[row] += x[ja[k]] * a[k];
+
+    }
+
+    timer.stop();
+
+    mega_flops = double(n_flops) / 1.0e6 / timer.seconds();
+    std::cout << "Measured MFLOP/S naive CSR: " << yellow << mega_flops << reset << std::endl;
+/*
+    a.clear();
+    a.shrink_to_fit();
+    ia.clear();
+    ia.shrink_to_fit();
+    ja.clear();
+    ja.shrink_to_fit();
+
+    // min-fixed coo
+    int k = 4; // Freescale1: 16x16
+    int l = 4;
+    sort_wrt_blocks_lex(elements, k, l);
+
+    std::vector<double> coo_vals(nnz_stored);
+    uint64_t coo_str_size = (uint64_t)(k + l) * nnz_stored / 8 + 1;
+    std::vector<uint8_t> coo_str(coo_str_size);
+
+    auto bi_2 = block_index_powers(elements.front(), k, l);
+    for (auto iter = elements.cbegin() + 1; iter != elements.cend(); ++iter) {
+        auto bi_2 = block_index_powers(*iter, k, l);
+        if (bi_2 != bi_1) {
+        }
+    }
+*/
 }
